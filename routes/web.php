03f5b88e503079d7cf1e25183da51950a3572da0
jakariya0dev <jhalhadi@gmail.com>
2023-08-24 15:29:02 +0600

    admin login system added

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Http\Controllers\addNewUserData;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+
+// Front end Route
+Route::view('/', 'frontend.app')->name('home');
+Route::view('/tos', 'frontend.about')->name('tos');
+Route::view('/p-policy', 'frontend.about')->name('p.policy');
+
+
+// User Route
+Route::view('/user-form', 'backend.user.user-form')->name('user.form');
+Route::get('/user-profile', [UserController::class, 'userLogin'])->name('user.profile');
+
+
+// Admin Route
+
+Route::middleware('auth')->group(function (){
+    Route::get('/admin-login', [UserController::class, 'getAllProfile'])->name('admin.login');
+    Route::view('/new-profile', 'backend.admin.new-form')->name('profile.new');
+    Route::get('/all-profile', [UserController::class, 'getAllProfile'])->name('profile.all');
+    Route::get('/view-profile/{id}', [UserController::class, 'getSingleProfile'])->name('profile.view');
+    Route::post('/add-profile', [UserController::class, 'addNewProfile'])->name('profile.add');
+    Route::get('/delete-profile/{id}', [UserController::class, 'deleteProfile'])->name('profile.delete');
+    Route::get('/search-profile', [UserController::class, 'searchProfile'])->name('profile.search');
+    Route::get('/edit-profile/{id}', [UserController::class, 'editProfile'])->name('profile.edit');
+    Route::put('/update-profile', [UserController::class, 'updateProfile'])->name('profile.update');
+    
+});
+
+
+// Route::get('/w', function () {
+//     return view('welcome');
+// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
